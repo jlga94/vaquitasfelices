30c2fcf548278b97202828bf6d36b9b237e9e089
jose.gil <jose.gil@pucp.pe>
2017-05-06 21:03:01 -0500

    valores de usuario

--- usuario/models.py
+++ usuario/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+from django.contrib.auth.models import AbstractUser
+
 # Create your models here.
+class User(AbstractUser):
+	direccion = models.CharField(max_length=50,null=True,blank=True)
+	telefono = models.IntegerField(max_length=15,null=True,blank=True)
+	fecha_nacimiento = models.DateField(null=True,blank=True)
+
